624bd29379281b1d52112dc0dec9fc963fff8a60
Salem <salemmosbahi@gmail.com>
2016-03-19 15:32:41 +0100

    fixed socket

--- route/sockets.js
+++ route/sockets.js
@@ -1,17 +1,26 @@
 module.exports = function(io){
   io.on('connection',function(socket){
     console.log('one user connected ' + socket.id);
+
     socket.on('gps',function(data){
       var sockets = io.sockets.sockets;
-      console.log('data ' + data.latitude + ' ' + data.longitude);
+      var paquet = { 'token':data.token, 'socket':socket.id, 'latitude':data.latitude, 'longitude':data.longitude, 'working':data.working };
       sockets.forEach(function(sock){
         if(sock.id != socket.id){
-          sock.emit('gps',data);
+          sock.emit('gps',paquet);
         }
       });
     });
+
     socket.on('disconnect',function(){
       console.log('one user disconnected ' + socket.id);
+      var sockets = io.sockets.sockets;
+      var paquet = { 'socket':socket.id, 'working':false };
+      sockets.forEach(function(sock){
+        if(sock.id != socket.id){
+          sock.emit('gps',paquet);
+        }
+      });
     })
   });
 }
